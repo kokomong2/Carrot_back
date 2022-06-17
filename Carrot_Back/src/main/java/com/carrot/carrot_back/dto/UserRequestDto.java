3a07c85cbf5db010239da3d08540467ba4165e00
kokomong2 <ko9603@naver.com>
2022-06-17 15:04:04 +0900

    로그인 회원가입 구현

---
+++ Carrot_Back/src/main/java/com/carrot/carrot_back/dto/UserRequestDto.java
@@ -0,0 +1,13 @@
+package com.carrot.carrot_back.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+public class UserRequestDto {
+    private String username;
+    private String password;
+//    private String passwordCheck;
+    private String nickname;
+}
